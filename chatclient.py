a2db9f395fbf14ccd1ae73fc3ea32a3c9ad2994e
Joviro Rocha <joviro_neto@hotmail.com>
2022-12-23 15:34:09 -0800

    'A'

--- chatclient.py
+++ chatclient.py
@@ -4,6 +4,19 @@ import pickle
 import threading
 import const #- addresses, port numbers etc. (a rudimentary way to replace a proper naming service)
 
+def authentication():
+    i = 1
+    while(i < 4):
+        if(i == 2): print('You have just one more try!')
+        username = input('Enter your username: ')
+        password = input('Enter your password: ')
+        if(const.registry[username] and const.registry[username][2] == password):
+            print('Welcome ' + username + '\n')
+            return username
+        print('Wrong username or password!')
+        if(i == 2): sys.exit(1)
+        i= i + 1
+
 class RecvHandler(threading.Thread):
   def __init__(self, sock):
     threading.Thread.__init__(self)
@@ -16,12 +29,12 @@ class RecvHandler(threading.Thread):
         #print('Server connected to me.')
         marshaled_msg_pack = conn.recv(1024)   # receive data from server
         msg_pack = pickle.loads(marshaled_msg_pack) # unmarshal message pack
-        print("MESSAGE: " + msg_pack[0] + " - FROM: " + msg_pack[1])
+        print("You got a new message!\nMESSAGE: " + msg_pack[0] + " - FROM: " + msg_pack[1] + '\n')
         conn.send(pickle.dumps("ACK")) # simply send the server an Ack to confirm
         conn.close()
     return # We need a condition for graceful termination
 
-me = str(sys.argv[1]) # User's name (as registered in the registry. E.g., Alice, Bob, ...)
+me = authentication()
 client_sock = socket(AF_INET, SOCK_STREAM) # socket for server to connect to this client
 my_ip = const.registry[me][0]   # If using a proper naming service, client should know its
 my_port = const.registry[me][1] # addresses (which it would register in the ns)
