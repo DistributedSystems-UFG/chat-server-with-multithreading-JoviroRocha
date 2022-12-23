7444cfbc61875dcc1547db88502c5de29bc3619e
Joviro Rocha <joviro_neto@hotmail.com>
2022-12-23 15:54:24 -0800

    'A'

--- const.py
+++ const.py
@@ -2,4 +2,4 @@ CHAT_SERVER_HOST = "172.31.67.168"
 CHAT_SERVER_PORT = 5678
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("172.31.78.108",5678,4321), "Bob":("172.31.77.0",5678,5533)}
+registry = {"Alice":("172.31.78.108",5678,"4321"), "Bob":("172.31.77.0",5678,"5533")}
