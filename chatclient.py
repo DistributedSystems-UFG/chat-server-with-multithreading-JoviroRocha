13839afd8e8955cc8ba1234706be1c95b94576db
Ubuntu <ubuntu@ip-172-31-77-0.ec2.internal>
2022-12-24 00:22:32 +0000

    a

--- chatclient.py
+++ chatclient.py
@@ -10,12 +10,12 @@ def authentication():
         if(i == 2): print('\n\nYou have just one more try!\n\n')
         username = input('Enter your username: ')
         password = input('Enter your password: ')
-        if(not const.registry[username] or not (const.registry[username][2] == password)):
-            print('\nWrong username or password!\n')
-            if(i == 2): sys.exit(1)
-        else:
-            print('Welcome ' + username + '\n')
-            return username   
+        if(const.registry.get(username, 'default_error') != 'default_error'):
+            if(const.registry[username][2] == password):
+             print('Welcome ' + username + '\n')
+             return username
+        print('\nWrong username or password!\n')
+        if(i == 2): sys.exit(1)
         i= i + 1
 
 class RecvHandler(threading.Thread):
